793c714e41d7dad6b490ae96f411dbf93faf3562
DariaAleshina <ddaleshina@gmail.com>
2026-04-01 16:21:56 +0200

    draft component structure

--- frontend/src/components/InputField/InputField.tsx
+++ frontend/src/components/InputField/InputField.tsx
@@ -1,2 +1,31 @@
 import type { InputFieldProps } from './InputField.types';
-import { inputFieldStyles } from './InputField.styles';
+import {} from './InputField.styles';
+
+const ERROR_MESSAGE = 'This field is mandatory.';
+
+const InputField = ({
+  label,
+  state,
+  error = ERROR_MESSAGE,
+  required,
+  showPasswordToggle = false,
+  ...rest // type, placeholder, value, onChange, onBlur...
+}: InputFieldProps) => {
+  return (
+    <div className="flex flex-col gap-2.5 text-input-label">
+      <label htmlFor={label}>
+        {label}
+        {required && <span className="text-input-error text-lg">*</span>}
+      </label>
+      <input
+        id={label}
+        required={required}
+        {...rest}
+        className="text-input-text border border-input-label rounded-base p-5 placeholder:text-input-placeholder"
+      />
+      {state === 'error' && error && <p className="text-input-error text-xs">{error}</p>}
+    </div>
+  );
+};
+
+export default InputField;
--- frontend/src/components/InputField/InputField.types.ts
+++ frontend/src/components/InputField/InputField.types.ts
@@ -1,3 +1,8 @@
-interface InputFieldProps {}
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  state: string; // 'default' or 'error'
+  label: string; // any passed
+  error?: string; // any passed
+  showPasswordToggle?: boolean;
+}
 
 export type { InputFieldProps };
