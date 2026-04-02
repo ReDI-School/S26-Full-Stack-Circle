4b8d049174ca87c16de5c08952affef2b9391206
DariaAleshina <ddaleshina@gmail.com>
2026-04-02 16:42:00 +0200

    refactor: define styles in seperate file via tv-function (slots)

--- frontend/src/components/InputField/InputField.styles.ts
+++ frontend/src/components/InputField/InputField.styles.ts
@@ -1,1 +1,19 @@
 import { tv } from 'tailwind-variants';
+
+export const inputFieldStyles = tv({
+  slots: {
+    wrapper: 'flex flex-col gap-2.5 text-input-label relative',
+    input: 'border rounded-base p-5 placeholder:text-input-placeholder',
+    errorText: 'text-input-error text-xs',
+    asterisk: 'text-input-error text-lg',
+    faEye: 'absolute bottom-6 right-4',
+  },
+  variants: {
+    hasError: {
+      true: {
+        input: 'border-input-warning',
+        faEye: 'text-input-warning',
+      },
+    },
+  },
+});
--- frontend/src/components/InputField/InputField.tsx
+++ frontend/src/components/InputField/InputField.tsx
@@ -1,6 +1,6 @@
 import type { InputFieldProps } from './InputField.types';
-import {} from './InputField.styles';
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { inputFieldStyles } from './InputField.styles';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useState } from 'react';
 
 const InputField = ({
@@ -10,31 +10,39 @@ const InputField = ({
   type,
   ...rest // placeholder, value, onChange, onBlur...
 }: InputFieldProps) => {
+  const [passwordVisiblity, setPasswordVisiblity] = useState(false);
+  const [textType, setTextType] = useState(type);
+  const isPassword = type === 'password';
 
-    const [passwordVisiblity, setPasswordVisiblity] = useState(false);
-    const [textType, setTextType] = useState(type);
-    const isPassword = type === "password"
+  // retrieving individual component styles
+  const { wrapper, input, errorText, asterisk, faEye } = inputFieldStyles({ hasError: !!error });
 
-    const passwordToggle = () => {
-      setPasswordVisiblity(!passwordVisiblity)
-      setTextType(textType === "password" ? "text" : "password")
-    }
-    
+  const passwordToggle = () => {
+    setPasswordVisiblity(!passwordVisiblity);
+    setTextType(textType === 'password' ? 'text' : 'password');
+  };
+
   return (
-    <div className="flex flex-col gap-2.5 text-input-label relative">
+    <div className={wrapper()}>
       <label htmlFor={label}>
         {label}
-        {required && <span className="text-input-error text-lg">*</span>}
+        {required && <span className={asterisk()}>*</span>}
       </label>
       <input
         id={label}
         required={required}
         type={isPassword ? textType : type}
+        className={input()}
         {...rest}
-        className={`${error ? "border-input-warning" : ""}  border rounded-base p-5 placeholder:text-input-placeholder`}
       />
-      {error && <p className="text-input-error text-xs">{error}</p>}
-      {type === 'password' && (<span onClick={()=>passwordToggle()} className="absolute bottom-6 right-4">{passwordVisiblity? <FaEye/> : <FaEyeSlash/>}</span>)}
+
+      {type === 'password' && (
+        <span onClick={() => passwordToggle()} className={faEye()}>
+          {passwordVisiblity ? <FaEye /> : <FaEyeSlash />}
+        </span>
+      )}
+
+      {error && <p className={errorText()}>{error}</p>}
     </div>
   );
 };
