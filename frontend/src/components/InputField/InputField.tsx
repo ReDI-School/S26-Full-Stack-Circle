c1600d3dbec18f43ffbc487eaa8b69fa006c17b0
Oways Othman <owayslfkf@gmail.com>
2026-04-02 15:28:05 +0200

    added-eye-icon-to-password-inputfield

--- frontend/src/components/InputField/InputField.tsx
+++ frontend/src/components/InputField/InputField.tsx
@@ -1,15 +1,27 @@
 import type { InputFieldProps } from './InputField.types';
 import {} from './InputField.styles';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from 'react';
 
 const InputField = ({
   label,
   error,
   required,
-  showPasswordToggle = false,
-  ...rest // type, placeholder, value, onChange, onBlur...
+  type,
+  ...rest // placeholder, value, onChange, onBlur...
 }: InputFieldProps) => {
+
+    const [passwordVisiblity, setPasswordVisiblity] = useState(false);
+    const [textType, setTextType] = useState(type);
+    const isPassword = type === "password"
+
+    const passwordToggle = () => {
+      setPasswordVisiblity(!passwordVisiblity)
+      setTextType(textType === "password" ? "text" : "password")
+    }
+    
   return (
-    <div className="flex flex-col gap-2.5 text-input-label">
+    <div className="flex flex-col gap-2.5 text-input-label relative">
       <label htmlFor={label}>
         {label}
         {required && <span className="text-input-error text-lg">*</span>}
@@ -17,10 +29,12 @@ const InputField = ({
       <input
         id={label}
         required={required}
+        type={isPassword ? textType : type}
         {...rest}
         className={`${error ? "border-input-warning" : ""}  border rounded-base p-5 placeholder:text-input-placeholder`}
       />
       {error && <p className="text-input-error text-xs">{error}</p>}
+      {type === 'password' && (<span onClick={()=>passwordToggle()} className="absolute bottom-6 right-4">{passwordVisiblity? <FaEye/> : <FaEyeSlash/>}</span>)}
     </div>
   );
 };
--- frontend/src/components/InputField/InputField.types.ts
+++ frontend/src/components/InputField/InputField.types.ts
@@ -1,7 +1,6 @@
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string; // any passed
   error?: string; // any passed
-  showPasswordToggle?: boolean;
 }
 
 export type { InputFieldProps };
