b84ea90d8a753421582e0693c1f22867eda1e81f
wms198 <wang.minshi@icloud.com>
2026-04-07 19:55:52 +0200

    refactor: to use valid text utility classes

--- frontend/src/components/Avatar/Avatar.styles.ts
+++ frontend/src/components/Avatar/Avatar.styles.ts
@@ -1,19 +1,12 @@
 import { tv } from 'tailwind-variants';
 
 export const avatarStyles = tv({
-  base: [
-    'flex',
-    'items-center',
-    'justify-center',
-    'rounded-full',
-    'bg-[var(--color-info)]',
-    'text-[var(--color-text-inverse)]',
-  ],
+  base: ['flex', 'items-center', 'justify-center', 'rounded-full', 'bg-info', 'text-text-inverse'],
   variants: {
     size: {
-      sm: 'size-8 --text-sm',
-      md: 'size-10 --text-base',
-      lg: 'size-16  --text-lg',
+      sm: 'size-8 text-sm',
+      md: 'size-10 text-base',
+      lg: 'size-16  text-lg',
     },
   },
 });
