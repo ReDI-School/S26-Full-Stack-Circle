415670db0d1d2658574f5ba649c473a4886bdfaa
DariaAleshina <ddaleshina@gmail.com>
2026-04-10 15:34:06 +0200

    add forwardRef/ref

--- frontend/src/components/Skeleton/Skeleton.tsx
+++ frontend/src/components/Skeleton/Skeleton.tsx
@@ -1,23 +1,22 @@
 'use client';
-
 import type { SkeletonProps } from './Skeleton.types';
 import { skeletonStyles } from './Skeleton.styles';
+import { forwardRef } from 'react';
 
-const Skeleton = ({
-  animation = 'wave',
-  width = '100%',
-  height = 16,
-  radius = 'md',
-  ...rest
-}: SkeletonProps) => {
-  return (
-    <span
-      {...rest}
-      className={skeletonStyles({ animation, radius })}
-      style={{ width, height }}
-      aria-hidden="true"
-    />
-  );
-};
+const Skeleton = forwardRef<HTMLSpanElement, SkeletonProps>(
+  ({ animation = 'wave', width = '100%', height = 16, radius = 'base', ...rest }, ref) => {
+    return (
+      <span
+        ref={ref}
+        {...rest}
+        className={skeletonStyles({ animation, radius })}
+        style={{ width, height }}
+        aria-hidden="true"
+      />
+    );
+  }
+);
+
+Skeleton.displayName = 'Skeleton';
 
 export default Skeleton;
