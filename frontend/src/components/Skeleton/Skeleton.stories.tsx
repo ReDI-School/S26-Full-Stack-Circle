11ead7158a4b6b4dc1f5b6c8517d8191cd501ac9
DariaAleshina <ddaleshina@gmail.com>
2026-04-10 15:41:47 +0200

    upd stories with w/h as numbers passes in props

--- frontend/src/components/Skeleton/Skeleton.stories.tsx
+++ frontend/src/components/Skeleton/Skeleton.stories.tsx
@@ -37,14 +37,14 @@ export const Default: Story = {
 export const Card: Story = {
   render: () => (
     <div className="flex flex-col gap-4">
-      <Skeleton height="70px" width="70px" radius="full" />
+      <Skeleton height={70} width={70} radius="full" />
       <Skeleton />
       <Skeleton width="75%" />
       <Skeleton width="50%" />
       <Skeleton width="60%" />
       <div className="flex gap-2">
-        <Skeleton height="25px" width="25px" />
-        <Skeleton height="25px" width="100px" />
+        <Skeleton height={25} width={25} />
+        <Skeleton height={25} width={100} />
       </div>
     </div>
   ),
