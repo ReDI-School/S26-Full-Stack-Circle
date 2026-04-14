bd5d60770c3db0ce2356a855be193c0462ca8875
wms198 <wang.minshi@icloud.com>
2026-04-14 10:51:47 +0200

    maintain code quality

--- frontend/src/components/UserArea/UserArea.stories.tsx
+++ frontend/src/components/UserArea/UserArea.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import UserArea from './UserArea';
@@ -11,7 +12,30 @@ const meta: Meta<typeof UserArea> = {
 export default meta;
 type Story = StoryObj<typeof UserArea>;
 
-export const Default: Story = {
+export const Expanded: Story = {
+  render: (args) => {
+    //XXX: no better way to expand without changeing interface
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        const trigger = document.querySelector('[aria-haspopup="true"][aria-expanded="false"]');
+        if (trigger) {
+          trigger.click();
+          clearInterval(interval);
+        }
+      }, 50);
+
+      return () => clearInterval(interval);
+    }, []);
+
+    return <UserArea {...args} />;
+  },
+  args: {
+    userName: 'George Michel',
+    avatarInitials: 'GM',
+  },
+};
+
+export const Collapsed: Story = {
   args: {
     userName: 'Lucy Lu',
     avatarInitials: 'LL',
