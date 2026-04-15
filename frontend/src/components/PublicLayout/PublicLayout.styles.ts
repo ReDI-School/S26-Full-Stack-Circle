d629980833befd98ae0453995ecb87eb965aa4bf
Maksim <maksim.t.kh@gmail.com>
2026-04-15 21:25:53 +0200

    feat( component): add left sidebar with SidebarImage in PublicLayout

--- frontend/src/components/PublicLayout/PublicLayout.styles.ts
+++ frontend/src/components/PublicLayout/PublicLayout.styles.ts
@@ -2,6 +2,7 @@ import { tv } from 'tailwind-variants';
 
 export const publicLayoutStyles = tv({
   slots: {
-      main: 'flex h-screen overflow-hidden',
+    main: 'flex h-screen overflow-hidden',
+    sidebar: 'hidden md:block w-120',
   },
 });
