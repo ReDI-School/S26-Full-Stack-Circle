aade87161624d373581ce96d761f59e1444dcd69
AlionaKolomyitseva <aliona.kolomyitseva@gmail.com>
2026-04-15 14:56:19 +0200

    feat: style UserArea with tailwind

--- frontend/src/components/UserArea/UserArea.tsx
+++ frontend/src/components/UserArea/UserArea.tsx
@@ -1,13 +1,15 @@
 import { UserAreaProps } from './UserArea.types';
 import { Avatar } from '../Avatar';
+import { CaretDownIcon } from '@phosphor-icons/react';
 
 const UserArea = ({ userName, avatarInitials }: UserAreaProps) => {
   return (
-    <div>
-      <div>
+    <div className="inline-flex items-center gap-2.5 rounded-full bg-surface px-3 py-2 cursor-pointer hover:bg-gray-100">
+      <div className="shrink-0">
         <Avatar size="md" initials={avatarInitials} />
       </div>
-      <div>{userName}</div>
+      <div className="text-base font-medium text-[#707070]">{userName}</div>
+      <CaretDownIcon size={18} className="text-[#707070]" />
     </div>
   );
 };
