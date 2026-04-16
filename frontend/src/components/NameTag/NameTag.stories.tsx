1b08c2479a22a2f39a7c030fb768a8d4b10c61e2
Tatiana Todoseva <59057635+Tatianatod@users.noreply.github.com>
2026-04-16 19:00:36 +0200

    fix: nextjs import in storybook

--- frontend/src/components/NameTag/NameTag.stories.tsx
+++ frontend/src/components/NameTag/NameTag.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import NameTag from './NameTag';
 
 const meta: Meta<typeof NameTag> = {
