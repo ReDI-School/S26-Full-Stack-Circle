2fee9678d8618b9a4b7a907e12fb03e020f152cc
Reda Kh <cr_kherfallah@esi.dz>
2026-04-16 16:07:17 +0200

    removing the layout data type

--- frontend/src/components/ProfileCard/ProfileCard.types.ts
+++ frontend/src/components/ProfileCard/ProfileCard.types.ts
@@ -18,11 +18,6 @@ interface ProfileCardProps {
    * The number of events the user has participated in. This number is displayed in the brief activity summary.
    */
   participatedEvents: number;
-
-  /**
-   * The layout switches between horizontal (desktop) and stacked+centered (mobile)
-   */
-  layout?: 'desktop' | 'mobile';
 }
 
 export type { ProfileCardProps };
