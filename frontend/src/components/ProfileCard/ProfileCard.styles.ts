ba2ba14ee8350a34d3171adee38ad332c2aa982d
Fabio Rodrigues de Oliveira <fabio.rodriguesdeoliveira@zvoove.com>
2026-04-17 10:20:13 +0200

    chore: quick fixes

--- frontend/src/components/ProfileCard/ProfileCard.styles.ts
+++ frontend/src/components/ProfileCard/ProfileCard.styles.ts
@@ -3,17 +3,17 @@ import { tv } from 'tailwind-variants';
 export const profileCardStyles = tv({
   base: [
     'w-full',
+    'text-text-tertiary',
+    'heading-text-tertiary',
     'flex',
     'flex-col',
     'items-center',
     'justify-center',
     'gap-5',
-    'text-center',
 
     // Desktop behavior
+    'text-center',
     'md:flex-row',
-    'md:items-center',
-    'md:justify-start',
     'md:gap-7',
     'md:text-left',
   ],
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,7 @@ export * from './Button';
 export * from './StickyButton';
 export * from './InputField';
 export * from './Layout';
+export * from './ProfileCard';
+export * from './Card';
+export * from './Avatar';
+export * from './InfoBox';
