9593326658bb59ce2db1dff6bd13b311fe990d87
Maksim <maksim.t.kh@gmail.com>
2026-04-19 22:00:31 +0200

    refactor(component): update PublicLayout layout styles

--- frontend/src/components/PublicLayout/PublicLayout.styles.ts
+++ frontend/src/components/PublicLayout/PublicLayout.styles.ts
@@ -3,9 +3,7 @@ import { tv } from 'tailwind-variants';
 export const publicLayoutStyles = tv({
   slots: {
     main: 'flex h-screen overflow-hidden',
-    sidebar: 'hidden md:block w-[480px]',
-    content: 'flex-1 bg-[#F5F5F5] flex flex-col',
-    nav: 'w-full flex justify-end items-center gap-2 px-6 py-4',
-    body: 'flex flex-1 items-center justify-center px-6',
+    sidebar: 'hidden lg:block w-[480px]',
+    content: 'flex-1 flex items-center justify-center',
   },
 });
