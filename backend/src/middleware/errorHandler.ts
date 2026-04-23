c6204e2c672fac0a8e16660e5b680b150c572784
DariaAleshina <ddaleshina@gmail.com>
2026-04-23 12:41:35 +0200

    add notFound & errorHandler functions

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function notFound(req: Request, res: Response) {
+  res.status(404).json({ error: 'Page not found' });
+}
+
+export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Something went wrong' });
+}
