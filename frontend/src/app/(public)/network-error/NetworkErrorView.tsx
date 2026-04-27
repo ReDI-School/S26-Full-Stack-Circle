224eb671fdd86a15db242ddbdb04849e68df5a58
Serhii SYRYTSIA <ssda.blablaman@gmail.com>
2026-04-27 11:33:09 +0200

    refactor: simplify NetworkError structure

--- frontend/src/app/(public)/network-error/NetworkErrorView.tsx
+++ frontend/src/app/(public)/network-error/NetworkErrorView.tsx
@@ -1,20 +1,20 @@
 'use client';
 
 import { Button } from '../../../components';
-import { networkErrorStyles } from './NetworkError.styles';
+import { useRouter } from 'next/navigation';
 
 const NetworkErrorView = () => {
-  const styles = networkErrorStyles();
+  const router = useRouter();
 
   const handleRefresh = () => {
-    window.location.reload();
+    router.refresh();
   };
 
   return (
-    <div className={styles.container()}>
-      <div className={styles.textWrapper()}>
-        <h1 className={styles.title()}>Something went wrong</h1>
-        <p className={styles.description()}>
+    <div className="flex flex-col items-center justify-center gap-8 max-w-md text-center lg:items-start lg:text-left">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-normal text-text-primary">Something went wrong</h1>
+        <p className="text-lg font-normal text-text-secondary">
           Seems like we are having trouble in our servers, please try to refresh the page.
         </p>
       </div>
