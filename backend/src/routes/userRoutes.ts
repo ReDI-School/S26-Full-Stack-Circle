d48ee7ec014e52bdfa7d2937992c1018ff8c9793
Reda Kh <cr_kherfallah@esi.dz>
2026-04-30 16:50:32 +0200

    remove the header's verification for the user signup

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -7,7 +7,7 @@ const userRouter = Router();
 
 userRouter.get('/', (req, res) => userController.getUsers(req, res));
 userRouter.get('/:id', (req, res) => userController.getUserById(req, res));
-userRouter.post('/', authenticate, (req, res) => userController.createUser(req, res));
+userRouter.post('/', (req, res) => userController.createUser(req, res));
 userRouter.put('/:id', authenticate, (req, res) => userController.updateUser(req, res));
 userRouter.delete('/:id', authenticate, (req, res) => userController.deleteUser(req, res));
 
