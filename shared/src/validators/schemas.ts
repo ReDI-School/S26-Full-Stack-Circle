48270a06e9232dc1f48734fec9f0ad4e569f01bb
DariaAleshina <ddaleshina@gmail.com>
2026-05-06 17:57:10 +0200

    add rules with customised errors for event schema

--- shared/src/validators/schemas.ts
+++ shared/src/validators/schemas.ts
@@ -1,26 +1,19 @@
 import { z } from 'zod';
 
+const trimmedString = z.string().trim();
 const errorRequiredField = (field: string) => `${field} is required.`;
-
-//TODO: add data sanitation to avoid melicious inputs, e.g. trim strings, escape special characters, etc.
 
 const registerSchema = z.object({
   email: z.email().trim().toLowerCase(),
 
-  firstName: z
-    .string()
-    .trim()
-    .min(1, { error: errorRequiredField('First name') }),
+  firstName: trimmedString.min(1, { error: errorRequiredField('First name') }),
 
-  lastName: z
-    .string()
-    .trim()
-    .min(1, { error: errorRequiredField('Last name') }),
+  lastName: trimmedString.min(1, { error: errorRequiredField('Last name') }),
 
   password: z
     .string()
-    .trim()
     .min(8, { error: 'Password must be at least 8 characters' })
+    .max(100, { error: 'Password must be at most 100 characters' }) // arbitrary DoS guard for bcrypt
     .regex(/[A-Z]/, { error: 'Password must contain an uppercase letter' })
     .regex(/[a-z]/, { error: 'Password must contain a lowercase letter' })
     .regex(/[0-9]/, { error: 'Password must contain a digit' })
@@ -31,28 +24,25 @@ const registerSchema = z.object({
 
 const loginSchema = z.object({
   email: z.email().trim().toLowerCase(),
-  // TODO: do i need a password rule here at login?
-  password: z.string().min(8),
+  password: z
+    .string()
+    .min(1, { error: errorRequiredField('Password') })
+    .max(100), // arbitrary DoS guard for bcrypt
 });
 
 const createEventSchema = z.object({
-  // TODO: add more specific validation rules, e.g. title and description min/max length
-  // TODO: add missing error messages for each field, e.g. "Capacity must be a positive integer", etc.
-  title: z
-    .string()
-    .trim()
-    .min(1, { error: errorRequiredField('Event title') }),
+  title: trimmedString
+    .min(3, { error: 'Event title must be at least 3 characters' })
+    .max(100, { error: 'Event title must be at most 100 characters' }),
 
-  description: z
-    .string()
-    .trim()
-    .min(1, { error: errorRequiredField('Event description') }),
+  description: trimmedString
+    .min(10, { error: 'Event description must be at least 10 characters' })
+    .max(2000, { error: 'Event description must be at most 2000 characters' }),
 
   capacity: z
-    .number()
-    .int()
-    .positive()
-    .min(1, { error: errorRequiredField('Capacity') }),
+    .number({ error: errorRequiredField('Capacity') })
+    .int({ error: 'Capacity must be a whole number' })
+    .positive({ error: 'Capacity must be a positive number' }),
 
   date: z.iso.date({ error: 'Invalid date format' }), // expected format YYYY-MM-DD from <input type="date">
 
