277f3257eaa2b5ddd75a2e37702ac6e928361ea7
Maksim <maksim.t.kh@gmail.com>
2026-05-13 22:06:58 +0200

    feat: add UserDTO

---
+++ backend/src/dto/user.dto.ts
@@ -0,0 +1,15 @@
+import { User } from '@prisma/client';
+
+export class UserDTO {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.email = user.email;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+  }
+}
