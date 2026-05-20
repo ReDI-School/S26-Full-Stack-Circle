80ea5bc9bd223a8541d5832e79f242438158fca9
santi-b-b <sbenitezbeas@gmail.com>
2026-05-20 11:37:49 +0200

    Improving invalid filter error validation.

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -1,36 +1,55 @@
 import { Request, Response } from 'express';
 import { EventService } from 'src/services/eventService.js';
-import { NextFunction } from 'express';
 
 const eventService = new EventService();
 
 type EventFilter = 'upcoming' | 'past';
 
-function parseEventFilter(value: unknown): EventFilter | undefined {
-  if (value === undefined) return undefined;
-  if (value === 'upcoming' || value === 'past') return value;
+function parseEventFilter(value: unknown): {
+  isValid: boolean;
+  filter?: EventFilter;
+} {
+  if (value === undefined) {
+    return {
+      isValid: true,
+      filter: undefined,
+    };
+  }
 
-  throw new Error('INVALID_EVENT_FILTER');
+  if (value === 'upcoming' || value === 'past') {
+    return {
+      isValid: true,
+      filter: value,
+    };
+  }
+
+  return {
+    isValid: false,
+  };
 }
 
 export class EventController {
-  async getEvents(req: Request, res: Response, next: NextFunction) {
-    try {
-      const filter = parseEventFilter(req.query.filter);
-      const events = await eventService.getEvents(filter);
-      res.json({ events });
-    } catch (err) {
-      console.log('Error caught:', err);
-      console.log('Error type:', err instanceof Error);
-      console.log('Error message:', (err as Error).message);
-      next(err);
+  async getEvents(req: Request, res: Response) {
+    const { isValid, filter } = parseEventFilter(req.query.filter);
+
+    if (!isValid) {
+      return res.status(400).json({
+        error: 'Invalid event filter',
+      });
     }
+    const events = await eventService.getEvents(filter);
+    res.json({ events });
   }
+
   getEventById = async (req: Request, res: Response) => {
     const { id } = req.params;
+
     const event = await eventService.getEventById(id);
+
     if (!event) {
-      return res.status(404).json({ error: 'Event not found' });
+      return res.status(404).json({
+        error: 'Event not found',
+      });
     }
 
     res.json({ event });
