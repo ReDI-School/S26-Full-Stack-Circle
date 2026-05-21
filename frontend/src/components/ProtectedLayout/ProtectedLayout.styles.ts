22c832ef37ba353f3588a63df3fa8a849c51a1f0
Reda Kh <cr_kherfallah@esi.dz>
2026-05-21 16:17:49 +0200

    remove the sticky position as well as the border for the headerBar and add the necessary padding for the content

--- frontend/src/components/ProtectedLayout/ProtectedLayout.styles.ts
+++ frontend/src/components/ProtectedLayout/ProtectedLayout.styles.ts
@@ -4,12 +4,7 @@ export const protectedLayoutStyles = tv({
   slots: {
     main: ['flex', 'flex-col', 'h-screen', 'overflow-hidden'],
     headerBar: [
-      'sticky',
-      'top-0',
-      'left-0',
-      'right-0',
       'min-h-16',
-      'z-50',
       'flex',
       'justify-between',
       'items-center',
@@ -17,9 +12,7 @@ export const protectedLayoutStyles = tv({
       'lg:px-12.5',
       'py-4',
       'bg-white',
-      'border-b',
-      'border-gray-200',
     ],
-    content: ['flex-1', 'overflow-y-auto'],
+    content: ['flex-1', 'overflow-y-auto', 'px-7.5', 'lg:px-12.5', 'py-4'],
   },
 });
