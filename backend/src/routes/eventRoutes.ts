bec6cdecbf42e16b7940d5b6693789aae7e72e83
santi-b-b <sbenitezbeas@gmail.com>
2026-05-25 12:47:31 +0200

    Taking the next parameter from the eventrouter that is not used anymore

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -4,7 +4,7 @@ import { EventController } from 'src/controllers/eventController.js';
 const eventController = new EventController();
 const eventRouter = Router();
 
-eventRouter.get('/', (req, res, next) => eventController.getEvents(req, res, next));
+eventRouter.get('/', (req, res) => eventController.getEvents(req, res, next));
 eventRouter.get('/:id/attendees', (req, res) => eventController.getAttendees(req, res));
 eventRouter.get('/:id', (req, res) => eventController.getEventById(req, res));
 
