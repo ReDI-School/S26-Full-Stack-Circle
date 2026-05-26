8109dee902ed33ca28a17f0f2e22e730fb0c1459
Maksym <maksim.t.kh@gmail.com>
2026-05-26 22:05:39 +0200

    fix deleteEvent method

--- backend/src/services/eventService.ts
+++ backend/src/services/eventService.ts
@@ -31,9 +31,12 @@ export class EventService {
       where: { id },
     });
   }
+  
   async deleteEvent(id: string) {
     return await prisma.event.delete({
       where: { id },
+    });
+  }
 
   async createEvent(
     organizerId: string,
