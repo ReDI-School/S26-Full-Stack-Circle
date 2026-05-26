ec40291cad35434ccffbd5a7098229a7750965a8
Maks Vintskovskyi <intadmirmaks@gmail.com>
2026-05-26 16:44:14 +0200

    add validation

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { EventController } from '../controllers/eventController.js';
 import { AttendanceController } from '../controllers/attendanceController.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { updateEventSchema } from '../validators/schemas.js';
+import { validate } from '../middleware/validateUserInput.js';
 
 const eventController = new EventController();
 const attendanceController = new AttendanceController();
@@ -10,6 +12,8 @@ const eventRouter = Router();
 eventRouter.post('/:id/attend', authenticate, (req, res) => attendanceController.attend(req, res));
 eventRouter.get('/', (req, res) => eventController.getEvents(req, res));
 eventRouter.get('/:id', (req, res) => eventController.getEventById(req, res));
-eventRouter.put('/:id', authenticate, (req, res) => eventController.updateEvent(req, res));
+eventRouter.put('/:id', authenticate, validate(updateEventSchema), (req, res) =>
+  eventController.updateEvent(req, res)
+);
 
 export default eventRouter;
