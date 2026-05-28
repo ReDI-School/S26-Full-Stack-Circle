1e4d5ea3d26356251b47f2ca284ffe4c50fd7041
Reda Kh <cr_kherfallah@esi.dz>
2026-05-28 08:17:34 +0200

    Replaced the previous approach with routeDefinitions to provide a more scalable and JSX-friendly routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import Home from './app/pages/Home';
 import LoginPage from './app/sign-in/LoginPage';
 import SignIn from './app/sign-up/SignIn';
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/sign-in', element: <LoginPage /> },
-  { path: '/sign-up', element: <SignIn /> },
-  {},
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />,
+    <Route path="/sign-in" element={<LoginPage />} />,
+    <Route path="/sign-up" element={<SignIn />} />,
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
