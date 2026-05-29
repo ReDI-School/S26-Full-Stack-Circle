df3405fa4bd1903415e659988990529b412dd244
DariaAleshina <ddaleshina@gmail.com>
2026-05-29 13:52:26 +0200

    fix public layout style issue: make mobile not limited to screen height, and change overflow of content y axis to auto

--- frontend/src/components/PublicLayout/PublicLayout.styles.ts
+++ frontend/src/components/PublicLayout/PublicLayout.styles.ts
@@ -2,7 +2,7 @@ import { tv } from 'tailwind-variants';
 
 export const publicLayoutStyles = tv({
   slots: {
-    main: ['flex', 'flex-col', 'lg:flex-row', 'h-screen', 'overflow-hidden'],
+    main: ['flex', 'flex-col', 'lg:flex-row', 'md:h-screen', 'min-h-screen', 'overflow-y-auto'],
     logoMobile: ['flex', 'lg:hidden', 'pt-7.5', 'items-center', 'justify-center'],
     sidebar: ['hidden', 'lg:block', 'w-[480px]'],
     content: ['flex-1', 'flex', 'items-center', 'justify-center', 'p-7.5', 'lg:p-12.5'],
