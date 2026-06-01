ec65c085515cdc799a8ba1e1f37f0484d1ea7796
DariaAleshina <ddaleshina@gmail.com>
2026-06-01 21:53:46 +0200

    fix: scrollable area in public layout

--- frontend/src/components/PublicLayout/PublicLayout.styles.ts
+++ frontend/src/components/PublicLayout/PublicLayout.styles.ts
@@ -2,14 +2,16 @@ import { tv } from 'tailwind-variants';
 
 export const publicLayoutStyles = tv({
   slots: {
-    main: ['flex', 'flex-col', 'lg:flex-row', 'md:h-screen', 'min-h-screen', 'overflow-y-auto'],
+    main: ['flex', 'flex-col', 'lg:flex-row', 'h-screen', 'overflow-hidden'],
     logoMobile: ['flex', 'lg:hidden', 'pt-7.5', 'items-center', 'justify-center'],
     sidebar: ['hidden', 'lg:block', 'w-120'],
     content: [
+      'size-full',
+      'min-h-screen',
+      'overflow-y-auto',
       'relative',
       'p-7.5',
       'lg:p-12.5',
-      'size-full',
       'flex',
       'flex-1',
       'flex-col-reverse',
