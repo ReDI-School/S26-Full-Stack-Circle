636e6424a6a1c0f23130bef34ca6ac62fe3a1944
DariaAleshina <ddaleshina@gmail.com>
2026-06-01 21:24:33 +0200

    delete general public layout page that is not needed anymore

--- frontend/src/app/(public)/layout.tsx
+++
@@ -1,7 +0,0 @@
-interface PublicLayoutPageProps {
-  children: React.ReactNode;
-}
-
-const PublicLayoutPage = ({ children }: PublicLayoutPageProps) => <>{children}</>;
-
-export default PublicLayoutPage;
