04819f481915cf32b10b3dd3536528b69b7fb2ca
DariaAleshina <ddaleshina@gmail.com>
2026-06-01 21:26:49 +0200

    chore: remove code commented out

--- frontend/src/app/(public)/sign-up/page.tsx
+++ frontend/src/app/(public)/sign-up/page.tsx
@@ -22,18 +22,6 @@ const SignUp = () => {
   }, []);
 
   return (
-    // <div className="relative size-full flex-1 flex flex-col-reverse md:flex-col gap-8 items-center justify-center">
-    //   <div className="md:absolute top-0 right-0 :w-full flex gap-2 text-text-tertiary items-center justify-center md:justify-end">
-    //     Already have account?{' '}
-    //     <Link
-    //       href="/sign-in"
-    //       className="text-primary hover:text-primary-dark underline underline-offset-3 transition-colors"
-    //     >
-    //       SIGN IN
-    //     </Link>
-    //   </div>
-
-    // </div>
     <SignUpForm onSubmit={handleSubmit} isLoading={isLoading} serverError={serverError} />
   );
 };
