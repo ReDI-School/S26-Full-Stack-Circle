56355a9124e9d880fb04b314c0bab6d30c2d5d67
Reda Kh <cr_kherfallah@esi.dz>
2026-06-02 19:09:05 +0200

    moving the structural/heading markup to the layout for SignIn

--- frontend/src/app/(public)/sign-in/layout.tsx
+++ frontend/src/app/(public)/sign-in/layout.tsx
@@ -6,7 +6,17 @@ interface SignInLayoutProps {
 
 const SignInLayout = ({ children }: SignInLayoutProps) => (
   <PublicLayout navText="Don't have account?" navLinkLabel="SIGN UP" navLinkHref="/sign-up">
+        <div className="flex flex-col items-center justify-center gap-8 w-full">
+          <header className="flex flex-col gap-2 max-w-[400px] w-full">
+            <h2 className="text-center lg:text-left text-[28px] font-normal leading-[48px] text-text-primary">
+              Sign in to ReDi Events
+            </h2>
+            <p className="text-center lg:text-left text-[18px] font-normal leading-[24px] text-text-secondary">
+              Enter your details below.
+            </p>
+          </header>
     {children}
+    </div>
   </PublicLayout>
 );
 
--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -19,15 +19,6 @@ export const SignInForm = ({ onSubmit, isLoading, serverError }: SignInFormProps
   });
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 w-full">
-      <header className="flex flex-col gap-2 max-w-[400px] w-full">
-        <h2 className="text-center lg:text-left text-[28px] font-normal leading-[48px] text-text-primary">
-          Sign in to ReDi Events
-        </h2>
-        <p className="text-center lg:text-left text-[18px] font-normal leading-[24px] text-text-secondary">
-          Enter your details below.
-        </p>
-      </header>
       <form
         onSubmit={handleSubmit((data) => onSubmit(data))}
         className="flex flex-col gap-6 w-full max-w-[400px]"
@@ -59,6 +50,5 @@ export const SignInForm = ({ onSubmit, isLoading, serverError }: SignInFormProps
           </Button>
         </div>
       </form>
-    </div>
   );
 };
