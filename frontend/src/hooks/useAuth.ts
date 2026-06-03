ec8fcf00f49e2ba95c89e57a5aa91e6827f1e4e9
DariaAleshina <ddaleshina@gmail.com>
2026-06-03 14:20:12 +0200

    remove unused import (lint error)

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { loginRequest } from '@service/authService';
 import { LoginInput } from '@validators/schemas';
 
