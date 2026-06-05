a5b40ab4dbfc6d19804482702bd91e388ff3ee0c
Reda Kh <cr_kherfallah@esi.dz>
2026-06-05 23:05:32 +0200

    add get/ me route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,8 +3,10 @@ import { UserController } from '../controllers/userController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { validate } from '../middleware/validateUserInput.js';
 import { registerSchema } from '../validators/schemas.js';
+import { AuthController } from 'src/controllers/authController.js';
 
 const userController = new UserController();
+const authController = new AuthController();
 const userRouter = Router();
 
 userRouter.get('/', (req, res) => userController.getUsers(req, res));
@@ -12,5 +14,7 @@ userRouter.post('/', validate(registerSchema), (req, res) => userController.crea
 userRouter.get('/:id', (req, res) => userController.getUserById(req, res));
 userRouter.put('/:id', authenticate, (req, res) => userController.updateUser(req, res));
 userRouter.delete('/:id', authenticate, (req, res) => userController.deleteUser(req, res));
+
+userRouter.get('/me', authenticate, (req, res, next) => authController.me(req, res, next));
 
 export default userRouter;
