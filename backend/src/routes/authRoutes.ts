3bf0ca5c91afea8382b76bd95ae99333455b4574
Reda Kh <cr_kherfallah@esi.dz>
2026-06-05 23:48:45 +0200

    add /auth/me route inside authRoutes backend's file

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthController } from '../controllers/authController.js';
 import { loginSchema, registerSchema } from '../validators/schemas.js';
 import { validate } from 'src/middleware/validateUserInput.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const authController = new AuthController();
 const authRouter = Router();
@@ -13,5 +14,7 @@ authRouter.post('/login', validate(loginSchema), (req, res, next) =>
 authRouter.post('/register', validate(registerSchema), (req, res, next) =>
   authController.register(req, res, next)
 );
+
+authRouter.get('/me', authenticate, (req, res, next) => authController.me(req, res, next));
 
 export default authRouter;
