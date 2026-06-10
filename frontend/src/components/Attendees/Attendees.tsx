9a09e6f11d767952e0409128f71933e2ae2517fb
DariaAleshina <ddaleshina@gmail.com>
2026-06-10 16:23:13 +0200

    add empty state message to attendees component

--- frontend/src/components/Attendees/Attendees.tsx
+++ frontend/src/components/Attendees/Attendees.tsx
@@ -6,9 +6,11 @@ export default function Attendees({ attendees }: { attendees: string[] }) {
       <div className="flex flex-col gap-7.5">
         <p className="text-xl">ATTENDEES</p>
         <div className="flex gap-4 flex-wrap">
-          {attendees.map((attendee) => {
-            return <Tag label={attendee} key={attendees.indexOf(attendee)}></Tag>;
-          })}
+          {attendees.length === 0
+            ? <p className="text-ec-secondary">No attendees yet.</p>
+            : attendees.map((attendee) => (
+                <Tag label={attendee} key={attendees.indexOf(attendee)} />
+              ))}
         </div>
       </div>
     </Card>
