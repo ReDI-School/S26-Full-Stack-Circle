58a48c7b2edb6decdc5bd1999cca551e0fec3a97
santi-b-b <sbenitezbeas@gmail.com>
2026-06-11 00:14:06 +0200

    Fixing merge issues

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { createContext, useContext, useState } from 'react';
-import type { AuthUser } from '@service/authService';
+import type { AuthUser } from '@services/authService';
 
 interface AuthContextValue {
   authUser: AuthUser | null;
