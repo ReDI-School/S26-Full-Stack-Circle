8a884e0beed1c7d5b842214ee05bde69b6fa833e
DariaAleshina <ddaleshina@gmail.com>
2026-06-15 21:05:20 +0200

    adding appearance-none in input for mobile

--- frontend/src/components/InputField/InputField.styles.ts
+++ frontend/src/components/InputField/InputField.styles.ts
@@ -5,7 +5,7 @@ export const inputFieldStyles = tv({
     wrapper: 'flex flex-col gap-2.5 text-input-secondary w-full min-w-0',
     inputContainer: 'relative flex flex-col w-full',
     input:
-      'w-full min-w-0 border border-input-secondary rounded-base px-5 py-3 text-input-primary placeholder:text-input-tertiary',
+      'w-full min-w-0 border border-input-secondary rounded-base px-5 py-3 text-input-primary placeholder:text-input-tertiary appearance-none md:appearance-auto',
     errorText: 'text-input-error text-xs',
     asterisk: 'text-input-error text-lg',
     visibilityIcon: 'absolute top-1/2 -translate-y-1/2 right-5 text-input-secondary cursor-pointer',
